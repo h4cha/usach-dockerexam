fb730c430ef7f67197c892194e26981b1dff7bbe
h4cha <87454198+h4cha@users.noreply.github.com>
2022-03-03 17:07:16 -0500

    Update myexamdocker.sh

--- myexamdocker.sh
+++ myexamdocker.sh
@@ -4,7 +4,7 @@ ETC_HOSTS=/etc/hosts
 
 # Linea de comandos de ayuda (help) #
 display_help() {
-    echo "Mi primer script en bash para abrir un laboratorio de Pentesting(Docker based)"
+    echo "Mi primer script en bash para abrir un laboratorio de Pentesting basado en Docker"
     echo "Usage: $0 {list|status|info|start|stop} [projectname]" >&2
     echo
     echo " This scripts uses docker and hosts alias to make web apps available on localhost"
@@ -30,5 +30,30 @@ display_help() {
     echo "  Security Ninjas        - OpenDNS Security Ninjas AppSec Training"
     exit 1
 }
+
+############################################
+# Valida si Docker esta instalado y corriendo #
+############################################
+if ! [ -x "$(command -v docker)" ]; then
+  echo 
+  echo "Docker was not found. Please install docker before running this script."
+  echo "You can try the script: 	install_docker_kali_x64.sh"
+  echo "In the same repo at https://github.com/itboxltda/pentestlab"
+  exit
+fi
+
+if sudo service docker status | grep inactive > /dev/null
+then 
+	echo "Docker is not running."
+	echo -n "Do you want to start docker now (y/n)?"
+	read answer
+	if echo "$answer" | grep -iq "^y"; then
+		sudo service docker start
+	else
+		echo "Not starting. Script will not be able to run applications."
+	fi
+fi
+
+
 display_help
 #Pruebas
